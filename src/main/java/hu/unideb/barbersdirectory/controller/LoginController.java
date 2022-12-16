20bb7b04481f36fc766dfd563441976bf0b3406e
Hernádi Mihály <55395934+HernadiMihaly@users.noreply.github.com>
2022-12-16 01:19:45 +0100

    Repairing login method

--- src/main/java/hu/unideb/barbersdirectory/controller/LoginController.java
+++ src/main/java/hu/unideb/barbersdirectory/controller/LoginController.java
@@ -6,6 +6,11 @@ import hu.unideb.barbersdirectory.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +19,30 @@ public class LoginController {
 
     private final UserService userService;
     private final UserValidator userValidator;
+    private final AuthenticationManager authentication;
 
     @Autowired
-    public LoginController(UserService userService, UserValidator userValidator) {
+    public LoginController(UserService userService, UserValidator userValidator, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.userValidator = userValidator;
+        this.authentication= authenticationManager;
     }
 
-
-    @GetMapping("/login")
+    @PostMapping("/login")
     @CrossOrigin("http://localhost:4200/")
-    public String login() {
-        return "success";
+    public ResponseEntity<HttpStatus> login(@RequestBody User user) throws Exception {
+
+        Authentication authObject;
+        System.out.println(user.getEmail() + " " + user.getPassword());
+        try{
+            authObject = authentication.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail()
+                    , user.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authObject);
+        }catch (BadCredentialsException e){
+            throw new Exception("User not found! Please try with another credentials!");
+        }
+
+        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
     }
 
     @PostMapping("/register")
